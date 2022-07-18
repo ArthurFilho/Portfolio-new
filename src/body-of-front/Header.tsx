8cfa1a8ed827574d85be33b01fb982b7e801c644
Arthur <peraarthur2018@gmail.com>
2022-07-18 16:25:36 -0300

    atualization

--- src/body-of-front/Header.tsx
+++ src/body-of-front/Header.tsx
@@ -1,27 +1,28 @@
 import "../global.css"
 
+
  function Header(){
     return(
         <div>
             <div className=" title flex text-2xl  color: rgb(255 255 255) w-auto h-28 left-36 top-4 ">
                 <h1 className="logo">
-                    Arthur Timóteo Bezerra Pereira Filho
+                    Arthur Filho
                 </h1> 
-                
-                
+            
                 <div className="flex"> 
                 
                 <div className="headerx home">       
-                <a href="#"> Home </a>
+                 Home 
                 </div>
                 <div className="headerx">
-                <a href="#"> Projects </a>
+                 Projects
                 </div>
                 <div className="headerx">
                 <a href="mailto:peraarthur2018@hotmail.com"> Contact</a>   
                 </div>
                 
                 </div>
+            
             </div>
             
         </div>
--- src/body-of-front/endingPage.tsx
+++ src/body-of-front/endingPage.tsx
@@ -2,16 +2,7 @@
 
 function Endingpage(){
 return(<div>
-    <div className="about">
-    <h1 className="title">About me</h1>
-    <div className="boxtext">
-    <div className="imageplace">
-    <div className="textAbout"> 
-    Meu nome é Arthur, tenho 18 anos. Sou uma pessoa dinâmica, pontual, solícita e principalmente procuro me aprimorar e agregar conhecimento na área, estou nessa área para crescer e me desenvolver, sempre tentando melhorar a cada dia. Minhas skills atuais são React.js, JavaScript, HTML, CSS, BOOTSTRAP, Node(Básico) e estou estudando typescript e react native.
-    </div>
-    </div>
-    </div>
-    </div>
+    
     </div>
     )
 }
